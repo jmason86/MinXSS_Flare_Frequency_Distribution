bbd970e736c57bdd8d0307daf2fa07cd464c853a
James Paul Mason <jmason86@gmail.com>
2020-02-18 14:51:53 -0700

    Function to do time integration

--- minxss_make_flare_frequency_distribution.py
+++ minxss_make_flare_frequency_distribution.py
@@ -9,15 +9,29 @@ def integrate_spectrum_energy(irradiance, energy):
     return np.trapz(irradiance_masked, energy)
 
 
+def integrate_spectrum_time(irradiance, time_jd):
+    #irradiance_masked = np.ma.array(irradiance, mask=np.isnan(irradiance))  # Do I need this since the mask was already done in the energy integration? Don't want it because it drops the astropy units
+    time_seconds = (time_jd - time_jd[0]) * 86400 * u.second
+    return np.trapz(irradiance, time_seconds)
+
+
+def extract_time_jd(minxsslevel1):
+    return [minxsslevel1['time'][i]['jd'][0] for i in range(len(minxsslevel1))]
+
+
 # Read data
 data_path = '/Users/jmason86/Dropbox/minxss_dropbox/data/fm1/level1/'
 data = readsav('{}minxss1_l1_mission_length_v2.sav'.format(data_path))
 minxsslevel1 = data.minxsslevel1.x123[0].copy()
 
-# Sum all spectra -- one value per time
+# Integrate spectra across energy to produce one value per time
 irradiance = np.stack(minxsslevel1['irradiance'])
 energy = minxsslevel1[0]['energy']
 integrated_irradiance_energy = integrate_spectrum_energy(irradiance, energy) * (u.photon / u.second / u.centimeter**2)
+
+# Now integrate that across the times of all flares
+time_jd = extract_time_jd(minxsslevel1)
+bla = integrate_spectrum_time(integrated_irradiance_energy[0:50], time_jd[0:50])
 
 # Convert energy units from keV to erg
 energy = u.keV.to(u.erg, energy) * u.erg
