718b63a047fff27fd4802544cf29270381bc9d8c
James Mason <jmason86@gmail.com>
2023-02-27 16:19:22 -0500

    Updated code to accommodate new format of input

--- format_authors_list.py
+++ format_authors_list.py
@@ -1,15 +1,37 @@
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv('~/Desktop/Author List_November 15, 2022_13.47.csv')
-df.columns = ['First Name', 'Last Name', 'email', 'Name to appear in Journal', 'ORCID']
+df = pd.read_csv('~/Desktop/Student-authors.csv')
+df.columns = ['Name', 'ORCID']
 
-#filename = 'authors_phys1140.txt'
+def sort_by_last_name(names, orcids):
+    l2 = []
+
+    # create 2d list of names
+    for ele in names:
+        l2.append(ele.split())
+    
+    names_sorted = []
+    sorted_indices = sorted(range(len(l2)), key=lambda k: l2[k][-1])
+    orcids_sorted = [orcids[i] for i in sorted_indices]
+
+    # sort by last name
+    for ele in sorted(l2, key=lambda x: x[-1]):
+        names_sorted.append(' '.join(ele).title())
+    
+    #for i, ele in enumerate(sorted(l2, key=lambda x: x[-1]):
+    #    names_sorted.append(' '.join(ele))
+
+    return names_sorted, orcids_sorted
+
+names_sorted, orcids_sorted = sort_by_last_name(df['Name'], df['ORCID'])
+
 filename = '/Users/masonjp2/Dropbox/Apps/Overleaf/Flare Frequency Distribution (FFD)/authors_phys1140.tex'
 open(filename, 'w').close()
 
 for i in range(len(df.index)): 
-    name = df.iloc[i]['Name to appear in Journal']
-    orcid = df.iloc[i]['ORCID']
+    name = names_sorted[i]
+    orcid = orcids_sorted[i]
 
     with open(filename, 'a') as text_file:
         if str(orcid) == 'nan': 
